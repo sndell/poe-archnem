708a0547b2a2e29dae571af4feaeaf67c2a08b37
Benjamin Sundell <sundell.benjamin@gmail.com>
2022-03-13 13:40:54 +0100

    added stuff

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import GlobalStyles from './styled/Global';
+import test from './data/mods';
+import { useEffect, useState } from 'react';
+import styled from 'styled-components';
 
-function App() {
+import Item from './components/Item';
+
+const StyledHome = styled.div`
+  .item-container {
+    margin-left: 8px;
+    display: flex;
+  }
+`;
+
+const App = () => {
+  const [modList, setModList] = useState(test.map((item) => item.name));
+  const [selectedMods, setSelectedMods] = useState();
+  const [needed, setNeeded] = useState([]);
+
+  const addNeeded = (item) => {
+    setNeeded((old) => [...old, item]);
+  };
+
+  const getNeeded = () => {
+    const newArray = needed.map((item) => {
+      const count = needed.filter((name) => name === item).length;
+      return `${count}x ${item}`;
+    });
+    const uniq = [...new Set(newArray)];
+    return uniq;
+  };
+
+  const removeNeeded = (item) => {
+    var index = needed.indexOf(item);
+    needed.splice(index, 1);
+    setNeeded(needed);
+    console.log(index);
+  };
+
+  useEffect(() => {
+    console.log(modList);
+  }, []);
+
+  const handleSelect = (e) => {
+    setSelectedMods(test.filter((item) => item.name === e.target.value));
+    setNeeded([]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyles />
+      <StyledHome>
+        <h1>yelo</h1>
+        <select name="mods" id={selectedMods} onChange={handleSelect}>
+          {modList.map((name, index) => (
+            <option value={name} key={index}>
+              {name}
+            </option>
+          ))}
+        </select>
+        <div className="item-container">
+          {selectedMods &&
+            selectedMods.map((item) => (
+              <Item
+                item={item}
+                key={item.name}
+                add={addNeeded}
+                remove={removeNeeded}
+                doesHave={false}
+              />
+            ))}
+        </div>
+        <div className="needed-stats">
+          {Array.isArray(needed) &&
+            needed.length > 0 &&
+            getNeeded().map((item, index) => <h1 key={index}>{item}</h1>)}
+        </div>
+      </StyledHome>
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/components/Item.js
@@ -0,0 +1,139 @@
+import React, { useEffect, useState } from 'react';
+import { BsChevronDown, BsChevronUp } from 'react-icons/bs';
+import styled from 'styled-components';
+
+const StyledItem = styled.div`
+  margin-right: 8px;
+  .hidden {
+    /* display: none !important; */
+    height: 0;
+    overflow: hidden;
+  }
+
+  .item {
+    display: flex;
+    flex-direction: column;
+
+    .item-details {
+      height: 100%;
+      display: flex;
+      align-items: center;
+
+      svg {
+        cursor: pointer;
+        margin: 0 4px;
+      }
+
+      .img-container {
+        margin: 0 4px;
+        display: flex;
+        align-items: center;
+      }
+    }
+
+    .item-combination-container {
+      display: flex;
+
+      .item-combination-line-container {
+        width: 24px;
+        display: flex;
+        justify-content: center;
+        cursor: pointer;
+
+        .item-combination-line {
+          width: 1px;
+          background-color: black;
+        }
+      }
+    }
+  }
+`;
+
+const Item = (props) => {
+  const { item, add, remove, doesHave } = props;
+  const [isOpen, setIsOpen] = useState(true);
+  const [have, setHave] = useState(doesHave);
+
+  const toggleOpen = () => {
+    setIsOpen(!isOpen);
+  };
+
+  useEffect(() => {
+    setHave(doesHave);
+  }, [doesHave]);
+
+  useEffect(() => {
+    if (!have && !item.combination) {
+      add(item.name);
+      console.log('tes');
+    }
+    if (have && !item.combination) remove(item.name);
+  }, [have]);
+
+  const toggleHave = () => {
+    setHave(!have);
+  };
+
+  return (
+    <StyledItem>
+      <div className="item">
+        <div className="item-details">
+          {item.combination ? (
+            isOpen ? (
+              <BsChevronDown
+                style={{ color: 'black', fontSize: '16px' }}
+                onClick={toggleOpen}
+              />
+            ) : (
+              <BsChevronUp
+                style={{ color: 'black', fontSize: '16px' }}
+                onClick={toggleOpen}
+              />
+            )
+          ) : (
+            <span style={{ width: '24px' }} />
+          )}
+          <div className="img-container">
+            <img
+              src={require(`../data/images/${item.imgName}`)}
+              alt={item.name}
+              height={24}
+              width={24}
+            />
+          </div>
+          <h1 onClick={toggleHave}>{have ? <s>{item.name}</s> : item.name}</h1>
+        </div>
+        {item.combination && (
+          <div
+            className={`${
+              isOpen
+                ? 'item-combination-container'
+                : 'item-combination-container hidden'
+            } `}
+          >
+            <div
+              className="item-combination-line-container"
+              onClick={toggleOpen}
+            >
+              <div className="item-combination-line" onClick={toggleOpen} />
+            </div>
+
+            <div className="item-combination">
+              {item.combination.map((item) => (
+                <Item
+                  item={item}
+                  key={item.name}
+                  add={add}
+                  remove={remove}
+                  doesHave={have}
+                />
+              ))}
+            </div>
+          </div>
+        )}
+      </div>
+    </StyledItem>
+  );
+};
+
+export default Item;
